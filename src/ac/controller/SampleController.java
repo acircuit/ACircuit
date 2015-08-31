242313a58b2971d3e3be154e853aa882bb5f563b
ukhatry <udaykhatry@advisorcircuit.com>
2015-08-31 11:26:31 +0530

    Log4j

--- src/ac/controller/SampleController.java
+++ src/ac/controller/SampleController.java
@@ -1,11 +1,13 @@
 package ac.controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.apache.log4j.Logger;
 
 /**
  * Servlet implementation class SampleController
@@ -13,7 +15,8 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/SampleController")
 public class SampleController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+	private static final Logger logger = Logger
+			.getLogger(SampleController.class);
     /**
      * @see HttpServlet#HttpServlet()
      */
